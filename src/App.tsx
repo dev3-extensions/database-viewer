ab32767e9b4c260312ceada86532c2fd4c2317ec
Aryan Prince <aryanprincesanty@icloud.com>
2023-04-27 00:51:41 +0100

    feat: Basic `JSON` data fetching and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,99 @@
-import { Tab, initTE } from "tw-elements";
+import { Plus, Search, Settings } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { Tab, initTE } from 'tw-elements'
 
-initTE({ Tab });
+import table from '../table.json'
 
-function App() {
-    return (
-        <>
-            <div className="min-h-[400px] w-[600px] bg-[#262626] text-white">
-                <div className="m-4 flex flex-col gap-4">
-                    <p className="text-2xl font-semibold">Database Viewer</p>
-                    <ul
-                        className="mr-4 flex list-none flex-col flex-wrap pl-0"
-                        role="tablist"
-                        data-te-nav-ref
-                    >
-                        <li
-                            role="presentation"
-                            className="flex-grow text-center"
-                        >
-                            <a
-                                href="#tabs-profile01"
-                                className="focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                                data-te-toggle="pill"
-                                data-te-target="#tabs-profile01"
-                                role="tab"
-                                aria-controls="tabs-profile01"
-                                aria-selected="false"
-                            >
-                                Profile
-                            </a>
-                        </li>
-                        <li
-                            role="presentation"
-                            className="flex-grow text-center"
-                        >
-                            <a
-                                href="#tabs-profile02"
-                                className="focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                                data-te-toggle="pill"
-                                data-te-target="#tabs-profile02"
-                                role="tab"
-                                aria-controls="tabs-profile02"
-                                aria-selected="false"
-                            >
-                                Profile
-                            </a>
-                        </li>
-                        <li
-                            role="presentation"
-                            className="flex-grow text-center"
-                        >
-                            <a
-                                href="#tabs-profile03"
-                                className="focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                                data-te-toggle="pill"
-                                data-te-target="#tabs-profile03"
-                                role="tab"
-                                aria-controls="tabs-profile03"
-                                aria-selected="false"
-                            >
-                                Profile
-                            </a>
-                        </li>
-                    </ul>
+initTE({ Tab })
 
-                    <div className="my-2">
-                        <div
-                            className="hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-                            id="tabs-profile01"
-                            role="tabpanel"
-                            aria-labelledby="tabs-home-tab01"
-                            data-te-tab-active
-                        >
-                            Tab 1 content
-                        </div>
-                        <div
-                            className="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-                            id="tabs-profile03"
-                            role="tabpanel"
-                            aria-labelledby="tabs-profile-tab03"
-                        >
-                            Tab 2 content
-                        </div>
-                        <div
-                            className="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-                            id="tabs-messages03"
-                            role="tabpanel"
-                            aria-labelledby="tabs-profile-tab03"
-                        >
-                            Tab 3 content
-                        </div>
-                        <div
-                            className="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-                            id="tabs-contact03"
-                            role="tabpanel"
-                            aria-labelledby="tabs-contact-tab03"
-                        >
-                            Tab 4 content
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+type Table = {
+  name: string
+  id: string
+  type: string
+  description: string
 }
 
-export default App;
+function App() {
+  const [data, setData] = useState<Table[]>([])
+
+  const parsedTableData = table.table.slice(1).map((row) => ({
+    id: row[0],
+    name: row[1],
+    type: row[2],
+    description: row[3],
+  }))
+  useEffect(() => {}, [])
+
+  return (
+    <>
+      <div className="h-[400px] w-[600px] overflow-y-auto bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
+        <div className="p-4 flex flex-col gap-4">
+          <div className="flex gap-6">
+            <p className="text-2xl font-bold">Database Viewer</p>
+            <div className="bg-neutral-800 rounded-full py-1 px-4 flex gap-3">
+              <button>
+                <Plus strokeWidth={2} size={18} />
+              </button>
+              <button>
+                <Search strokeWidth={2} size={16} />
+              </button>
+              <button>
+                <Settings strokeWidth={2} size={16} />
+              </button>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <div className="w-3/12">
+              <p className="bg-neutral-700 px-3 py-1 rounded text-center font-medium text-sm">
+                IndexedDB
+              </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-2">
+              <div className="bg-neutral-700 px-3 py-1 rounded text-center font-medium text-sm">
+                <p>Database (testing data here)</p>
+              </div>
+              <div className="flex gap-2">
+                <button
+                  className="bg-neutral-700 hover:bg-neutral-800 px-3 py-1 rounded"
+                  onClick={() => {
+                    setData(parsedTableData)
+                  }}
+                >
+                  Read data
+                </button>
+                <button
+                  className="bg-neutral-700 hover:bg-neutral-800 px-3 py-1 rounded"
+                  onClick={() => {
+                    setData([])
+                  }}
+                >
+                  Clear
+                </button>
+              </div>
+              <div className="flex flex-col text-xs gap-4">
+                <table>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Description</th>
+                  </tr>
+                  {data.map((item) => (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.type}</td>
+                      <td>{item.description}</td>
+                    </tr>
+                  ))}
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
